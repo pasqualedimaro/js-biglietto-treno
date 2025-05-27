7e2e4bdbf8b8a6fd7a84a2ab0a5b24e7bfcfc1bd
pasqualedimaro <pakydm15@gmail.com>
2025-05-27 17:49:02 +0200

    add user data

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 -va applicato uno sconto del 20% per i minorenni
 -va applicato uno sconto del 40% per gli over 65.
 -L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.*/
+
+// dati utenti
+
+let chilometri = Number(prompt("quanti chilometri deve percorrere?"));
+let age = Number(prompt("inserisca la sua età per avere uno sconto"));
+
+console.log(chilometri, age);
